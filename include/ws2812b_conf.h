bc6c9e03580b47c8fcc3ed2e8f5fe40e2083ad0b
jell <infinite.ft@gmail.com>
2017-06-05 16:39:31 +0800

    change DMA channel from channel 1 to channel 4

--- include/ws2812b_conf.h
+++ include/ws2812b_conf.h
@@ -42,21 +42,21 @@
 #define WS2812B_AHB_RCC         RCC_AHBPeriph_DMA1
 
 #define WS2812B_GPIO            GPIOB
-#define WS2812B_GPIO_PIN        GPIO_Pin_6
+#define WS2812B_GPIO_PIN        GPIO_Pin_7
 
 #define WS2812B_TIM             TIM4
-#define WS2812B_TIM_OCINIT      TIM_OC1Init
-#define WS2812B_TIM_OCPRELOAD   TIM_OC1PreloadConfig
-#define WS2812B_TIM_DMA_CC      TIM_DMA_CC1
-#define WS2812B_TIM_DMA_CCR     (WS2812B_TIM->CCR1)
+#define WS2812B_TIM_OCINIT      TIM_OC2Init
+#define WS2812B_TIM_OCPRELOAD   TIM_OC2PreloadConfig
+#define WS2812B_TIM_DMA_CC      TIM_DMA_CC2
+#define WS2812B_TIM_DMA_CCR     (WS2812B_TIM->CCR2)
 
 #define WS2812B_DMA             DMA1
-#define WS2812B_DMA_CHANNEL     DMA1_Channel1
-#define WS2812B_DMA_IRQ         DMA1_Channel1_IRQn
+#define WS2812B_DMA_CHANNEL     DMA1_Channel4
+#define WS2812B_DMA_IRQ         DMA1_Channel4_IRQn
 
-#define WS2812B_DMA_HANDLER     DMA1_Channel1_IRQHandler
-#define WS2812B_DMA_IT_TC       DMA1_IT_TC1
-#define WS2812B_DMA_IT_HT       DMA1_IT_HT1
+#define WS2812B_DMA_HANDLER     DMA1_Channel4_IRQHandler
+#define WS2812B_DMA_IT_TC       DMA1_IT_TC4
+#define WS2812B_DMA_IT_HT       DMA1_IT_HT4
 
 #define WS2812B_IRQ_PRIO        0
 #define WS2812B_IRQ_SUBPRIO     0
